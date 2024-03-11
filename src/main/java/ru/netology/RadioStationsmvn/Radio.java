244afad16512cbf96e9cceb0185053b538b1d181
Aleksandr Volik <Aleksandr_volik@mail.ru>
2024-03-11 19:06:44 +0300

    Radio improvement

--- src/main/java/ru/netology/RadioStationsmvn/Radio.java
+++ src/main/java/ru/netology/RadioStationsmvn/Radio.java
@@ -1,38 +1,76 @@
 package ru.netology.RadioStationsmvn;
 
 public class Radio {
+    private int maxRadioStationNumber = 9;
+    private int minRadioStationNumber = 0;
+    private int maxSoundVolume = 100;
+    private int minSoundVolume = 0;
+    private int numberOfRadioChannels = 10;
     private int currentRadioStationNumber;
     private int soundVolume;
+
+    public Radio(int maxRadioStationNumber, int minRadioStationNumber, int maxSoundVolume, int minSoundVolume) {
+        this.maxRadioStationNumber = maxRadioStationNumber;
+        this.minRadioStationNumber = minRadioStationNumber;
+        this.maxSoundVolume = maxSoundVolume;
+        this.minSoundVolume = minSoundVolume;
+    }
+
+    public Radio(int numberOfRadioChannels) {
+        minRadioStationNumber = 1;
+        maxRadioStationNumber = minRadioStationNumber + numberOfRadioChannels -1;
+        this.numberOfRadioChannels = numberOfRadioChannels;
+    }
+
+    public int getMaxRadioStationNumber() {
+        return maxRadioStationNumber;
+    }
+
+    public int getMinRadioStationNumber() {
+        return minRadioStationNumber;
+    }
+
+    public int getMaxSoundVolume() {
+        return maxSoundVolume;
+    }
+
+    public int getMinSoundVolume() {
+        return minSoundVolume;
+    }
+
+    public int getNumberOfRadioChannels() {
+        return numberOfRadioChannels;
+    }
 
     public int getCurrentRadioStationNumber() {
         return currentRadioStationNumber;
     }
 
     public void setCurrentRadioStationNumber(int newCurrentRadioStationNumber) {
-        if (newCurrentRadioStationNumber < 0) {
-            newCurrentRadioStationNumber = 9;
+        if (newCurrentRadioStationNumber < minRadioStationNumber) {
+            newCurrentRadioStationNumber = maxRadioStationNumber;
         }
-        if (newCurrentRadioStationNumber > 9) {
+        if (newCurrentRadioStationNumber > maxRadioStationNumber) {
             return;
         }
         currentRadioStationNumber = newCurrentRadioStationNumber;
     }
 
     public void setToMaxTemp() {
-        currentRadioStationNumber = 9;
+        currentRadioStationNumber = maxRadioStationNumber;
     }
 
     public void switchChannelsWithButtonNext() {
-        if (currentRadioStationNumber == 9) {
-            currentRadioStationNumber = 0;
+        if (currentRadioStationNumber == maxRadioStationNumber) {
+            currentRadioStationNumber = minRadioStationNumber;
         } else {
             currentRadioStationNumber++;
         }
     }
 
     public void switchChannelsWithButtonPrev() {
-        if (currentRadioStationNumber == 0) {
-            currentRadioStationNumber = 9;
+        if (currentRadioStationNumber == minRadioStationNumber) {
+            currentRadioStationNumber = maxRadioStationNumber;
         } else {
             currentRadioStationNumber--;
         }
@@ -43,29 +81,29 @@ public class Radio {
     }
 
     public void setSoundVolume(int newSoundVolume) {
-        if (newSoundVolume < 0) {
-            newSoundVolume = 0;
+        if (newSoundVolume < minSoundVolume) {
+            newSoundVolume = minSoundVolume;
         }
-        if (newSoundVolume > 100) {
-            newSoundVolume = 100;
+        if (newSoundVolume > maxSoundVolume) {
+            newSoundVolume = maxSoundVolume;
         }
         soundVolume = newSoundVolume;
     }
 
     public void setToMaxSoundVolume() {
-        soundVolume = 100;
+        soundVolume = maxSoundVolume;
     }
 
     public void increaseSoundVolumeWithButtonPlus() {
-        if (soundVolume < 100) {
+        if (soundVolume < maxSoundVolume) {
             soundVolume++;
         } else {
-            soundVolume = 100;
+            soundVolume = maxSoundVolume;
         }
     }
 
     public void increaseSoundVolumeWithButtonMinus() {
-        if (soundVolume > 0) {
+        if (soundVolume > minSoundVolume) {
             soundVolume--;
         }
     }
