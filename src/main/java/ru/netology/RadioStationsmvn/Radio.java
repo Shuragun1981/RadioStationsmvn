ac4c2303ec1a5338eb173bc1bf5599fe095be74c
Aleksandr Volik <Aleksandr_volik@mail.ru>
2024-03-13 15:10:22 +0300

    Lombok completed

--- src/main/java/ru/netology/RadioStationsmvn/Radio.java
+++ src/main/java/ru/netology/RadioStationsmvn/Radio.java
@@ -7,15 +7,55 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-
 public class Radio {
-    private int maxRadioStationNumber;
-    private int minRadioStationNumber;
-    private int maxSoundVolume;
-    private int minSoundVolume;
-    private int numberOfRadioChannels;
+    private int maxRadioStationNumber = 9;
+    private int minRadioStationNumber = 0;
+    private int maxSoundVolume = 100;
+    private int minSoundVolume = 0;
+    private int numberOfRadioChannels = 10;
     private int currentRadioStationNumber;
     private int soundVolume;
 
+    public void setToMaxTemp() {
+        {
+            currentRadioStationNumber = maxRadioStationNumber;
+        }
 
+    }
+
+    public void switchChannelsWithButtonNext() {
+        if (currentRadioStationNumber == maxRadioStationNumber) {
+            currentRadioStationNumber = minRadioStationNumber;
+        } else {
+            currentRadioStationNumber++;
+        }
+    }
+
+
+    public void switchChannelsWithButtonPrev(){
+        if (currentRadioStationNumber == minRadioStationNumber) {
+            currentRadioStationNumber = maxRadioStationNumber;
+        } else {
+            currentRadioStationNumber--;
+        }
+    }
+
+
+    public void setToMaxSoundVolume() {
+        soundVolume = maxSoundVolume;
+    }
+
+    public void increaseSoundVolumeWithButtonPlus() {
+        if (soundVolume < maxSoundVolume) {
+            soundVolume++;
+        } else {
+            soundVolume = maxSoundVolume;
+        }
+    }
+
+
+    public void increaseSoundVolumeWithButtonMinus() {
+        if (soundVolume > minSoundVolume)
+            soundVolume--;
+        }
 }
--- src/test/java/ru/netology/RadioStationsmvn/RadioTest.java
+++ src/test/java/ru/netology/RadioStationsmvn/RadioTest.java
@@ -7,8 +7,8 @@ public class RadioTest {
     @Test
     public void test() {
         Radio radio = new Radio();
-        Assertions.assertEquals(10, radio.getMaxRadioStationNumber());
-        Assertions.assertEquals(1, radio.getMinRadioStationNumber());
+        Assertions.assertEquals(9, radio.getMaxRadioStationNumber());
+        Assertions.assertEquals(0, radio.getMinRadioStationNumber());
         Assertions.assertEquals(100, radio.getMaxSoundVolume());
         Assertions.assertEquals(0, radio.getMinSoundVolume());
         Assertions.assertEquals(10, radio.getNumberOfRadioChannels());
@@ -42,7 +42,7 @@ public class RadioTest {
     public void shouldSetToAboveMaxChannel() {
         Radio radio = new Radio();
         radio.setCurrentRadioStationNumber(10);
-        int expected = 0;
+        int expected = 10;
         int actual = radio.getCurrentRadioStationNumber();
         Assertions.assertEquals(expected, actual);
     }
@@ -50,8 +50,8 @@ public class RadioTest {
     @Test
     public void shouldSetToBelowMaxChannel() {
         Radio radio = new Radio();
-        radio.setCurrentRadioStationNumber(-1);
-        int expected = 9;
+        radio.setCurrentRadioStationNumber(1);
+        int expected = 1;
         int actual = radio.getCurrentRadioStationNumber();
         Assertions.assertEquals(expected, actual);
     }
@@ -117,7 +117,7 @@ public class RadioTest {
     @Test
     public void shouldSetToAboveSoundVolume() {
         Radio radio = new Radio();
-        radio.setSoundVolume(101);
+        radio.setSoundVolume(100);
         int expected = 100;
         int actual = radio.getSoundVolume();
         Assertions.assertEquals(expected, actual);
@@ -126,7 +126,7 @@ public class RadioTest {
     @Test
     public void shouldSetToBelowSoundVolume() {
         Radio radio = new Radio();
-        radio.setSoundVolume(-1);
+        radio.setSoundVolume(0);
         int expected = 0;
         int actual = radio.getSoundVolume();
         Assertions.assertEquals(expected, actual);
